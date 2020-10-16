bb682d5928eb589531fbeaf955e75f54e2bcce64
z373522886 <373522886@qq.com>
2020-10-16 10:36:43 +0800

    # init

---
+++ src/Client.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ZengJJ\UcClient;
+
+class Client
+{
+    public $config;
+
+    public function __construct($config)
+    {
+        $this->config;
+    }
+
+    public function show()
+    {
+        echo $this->config;
+    }
+}

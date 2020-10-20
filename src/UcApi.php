e46ec30728ce1dfc290a9e31414fb47c9b8b8ab7
z373522886 <373522886@qq.com>
2020-10-20 17:35:50 +0800

    # 同步登出

--- src/UcApi.php
+++ src/UcApi.php
@@ -20,33 +20,6 @@ class UcApi extends ObjectBase
     }
 
     /**
-     * @param $uri
-     * @param array $params
-     * @param string $method
-     * @return array
-     * @throws \Exception
-     */
-    public function request($uri, $params = array(), $method = 'GET')
-    {
-        $response = $this->client->request($method, $this->getUrl($uri), [
-            'form_params' => $params
-        ]);
-
-        if (empty($response)) {
-            throw new \Exception('请求失败');
-        }
-        if ($response->getStatusCode() != 200) {
-            throw new \Exception('请求错误：' . $response->getStatusCode());
-        }
-        $result = json_decode($response->getBody(), true);
-        if (empty($result) || $result['code'] != 0) {
-            throw new \Exception(empty($result['message']) ? '数据有误' : $result['message']);
-        }
-
-        return $result['data'];
-    }
-
-    /**
      * 同步用户数据
      * @param $token
      * @return array
--- src/UcWeb.php
+++ src/UcWeb.php
@@ -3,12 +3,27 @@
 
 namespace ZengJJ\UcClient;
 
-use GuzzleHttp\Client;
-
 class UcWeb extends ObjectBase
 {
+    /**
+     * 第三方链接
+     * @param $app_key
+     * @param $app_url
+     * @return string
+     */
     public function thirdUrl($app_key, $app_url)
     {
         return $this->getUrl('/third?app_key=' . $app_key . '&app_url=' . urlencode($app_url));
+    }
+
+    /**
+     * 同步登出
+     * @param $token
+     * @return array
+     * @throws \Exception
+     */
+    public function syncLogout()
+    {
+        return $this->request('/third/logout');
     }
 }

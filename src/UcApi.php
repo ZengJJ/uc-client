f9e11438855c455192bbeff405f8f965d8122f9c
z373522886 <373522886@qq.com>
2020-10-20 15:23:14 +0800

    # 修改调用地址

--- src/UcApi.php
+++ src/UcApi.php
@@ -57,7 +57,7 @@ class UcApi extends ObjectBase
         if (empty($token)) {
             throw new \Exception('Token 不能为空');
         }
-        return $this->request('/user/sync', ['token' => $token]);
+        return $this->request('/user/info', ['token' => $token]);
     }
 
     /**

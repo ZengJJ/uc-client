30aeb3293f27e6490fcdfaaabdafd2d3db4149c0
z373522886 <373522886@qq.com>
2020-10-23 18:13:19 +0800

    # 消息推送完善

--- src/api/ApiApp.php
+++ src/api/ApiApp.php
@@ -10,21 +10,25 @@ class ApiApp extends UcApi
     /**
      * 推送企业微信消息
      * @param $app_token
-     * @param $data
+     * @param $phone
      * @param string $object
+     * @param string[] $properties
      * @return array
      * @throws \Exception
      */
-    public function sendWorkCorpMsg($app_token, $object = WorkCorpMsg::MSG_OBJECT_TEXT, $properties = WorkCorpMsg::MSG_OBJECT_TEXT_PROPERTIES)
+    public function sendWorkCorpMsg($app_token, $phone, $object = WorkCorpMsg::MSG_OBJECT_TEXT, $properties = WorkCorpMsg::MSG_OBJECT_TEXT_PROPERTIES)
     {
-        if (empty($app_token)) {
-            throw new \Exception('App Token 不能为空');
+        if (empty($app_token) || empty($phone)) {
+            throw new \Exception('App Token 和 phone 不能为空');
         }
 
         return $this->request('/app/send-work-corp-msg', [
             'app_token' => $app_token,
-            'object' => $object,
-            'properties' => $properties,
+            'phone' => $phone,
+            'data' => [
+                'object' => $object,
+                'properties' => $properties,
+            ]
         ]);
     }
 }

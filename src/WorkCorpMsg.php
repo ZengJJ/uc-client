e4f672d6026a840eba11447d026f603a780ddadb
z373522886 <373522886@qq.com>
2020-10-29 17:58:53 +0800

    # 代码完善

--- src/WorkCorpMsg.php
+++ src/WorkCorpMsg.php
@@ -12,9 +12,7 @@ class WorkCorpMsg
     ];
 
     const MSG_OBJECT_TEXT = '\EasyWeChat\Kernel\Messages\Text';
-    const MSG_OBJECT_TEXT_PROPERTIES = [
-        'content' => '内容'
-    ];
+    const MSG_OBJECT_TEXT_PROPERTIES = '内容';
 
     const MSG_OBJECT_TEXT_CARD = '\EasyWeChat\Kernel\Messages\TextCard';
     const MSG_OBJECT_TEXT_CARD_PROPERTIES = [

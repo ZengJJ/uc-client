486ad7a43a832285be0b8e74a08b6fb1263fcd30
z373522886 <373522886@qq.com>
2020-11-05 17:22:16 +0800

    # 新增接口

--- src/api/ApiUser.php
+++ src/api/ApiUser.php
@@ -7,6 +7,39 @@ use ZengJJ\UcClient\UcApi;
 class ApiUser extends UcApi
 {
     /**
+     * 刷新 TOKEN
+     * @param $token
+     * @param null $work_corp_id 未知企业微信时，可以指定（主要用作多企业绑定情况下的API登录）
+     * @return array
+     * @throws \Exception
+     */
+    public function tokenRefresh($token, $work_corp_id = null)
+    {
+        if (empty($token)) {
+            throw new \Exception('缺失必要参数');
+        }
+        return $this->request('/user/token-refresh', [
+            'app_key' => $this->app_key,
+            'token' => $token,
+            'work_corp_id' => $work_corp_id
+        ]);
+    }
+
+    /**
+     * 获取用户数据
+     * @param $token
+     * @return array
+     * @throws \Exception
+     */
+    public function info($token)
+    {
+        if (empty($token)) {
+            throw new \Exception('缺失必要参数');
+        }
+        return $this->request('/user/info', ['app_key' => $this->app_key, 'token' => $token]);
+    }
+
+    /**
      * 同步用户密码
      * @param $token
      * @param $pwd
@@ -42,19 +75,5 @@ class ApiUser extends UcApi
             'token' => $token,
             'pwd' => $pwd
         ]);
-    }
-
-    /**
-     * 获取用户数据
-     * @param $token
-     * @return array
-     * @throws \Exception
-     */
-    public function info($token)
-    {
-        if (empty($token)) {
-            throw new \Exception('缺失必要参数');
-        }
-        return $this->request('/user/info', ['app_key' => $this->app_key, 'token' => $token]);
     }
 }

09f915ff34eaa95715dad7c465f72757ae35c19b
z373522886 <373522886@qq.com>
2020-11-30 11:48:57 +0800

    # 定义应用参数

---
+++ src/AppParams.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ZengJJ\UcClient;
+
+class AppParams
+{
+    const TYPE_KDDS = 1;
+    const TYPE_QD = 2;
+    const TYPE_DISCUZ = 3;
+    const TYPE_ESTATE = 4;
+    const TYPE_NAMES = [
+        self::TYPE_KDDS => '中介系统',
+        self::TYPE_QD => '渠道系统',
+        self::TYPE_DISCUZ => 'DISCUZ系统',
+        self::TYPE_ESTATE => '开发商系统',
+    ];
+}

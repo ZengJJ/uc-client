41cdb87d93b766e233738ae52eb7b599728f6272
z373522886 <373522886@qq.com>
2021-01-20 09:37:45 +0800

    # 代码优化

--- src/ObjectBase.php
+++ src/ObjectBase.php
@@ -91,7 +91,7 @@ class ObjectBase
     {
         $pageURL = ($this->isHttps() ? 'https' : 'http') . "://";
         $pageURL .= $_SERVER["SERVER_NAME"];
-        $pageURL .= ($_SERVER["SERVER_PORT"] != 80) ? (':' . $_SERVER["SERVER_PORT"]) : '';
+        $pageURL .= in_array($_SERVER["SERVER_PORT"], [80, 443, '80', '443']) ? '' : (':' . $_SERVER["SERVER_PORT"]);
         $pageURL .= $_SERVER["REQUEST_URI"];
 
         return $pageURL;
